3c4dc8f770ae40fdfc89455d141a5decec32fdc9
github learning <pdhivya1@ford.com>
2022-11-15 14:33:43 +0530

    Hello

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        int i=5;
+        while(i<10) {
+            i++;
+
+            System.out.println("Hellooooo");
+        }
+    }
+}
